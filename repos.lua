5f54e614856a4b29a542e0bbf6b8dcc8d885d309
David Leal <halfpacho@gmail.com>
2023-06-26 18:54:29 +0000

    Use the Submodules Alternative tool

---
+++ repos.lua
@@ -0,0 +1,22 @@
+local repos = {
+    {
+        name = "cozy",
+        url = "https://github.com/minetest-mods/cozy.git",
+        dir = ""
+    },
+    {
+        name = "mobs_animal",
+        url = "https://github.com/panqkart/mobs_animal",
+        dir = ""
+    },
+    {
+        name = "mobs_redo",
+        url = "https://notabug.org/TenPlus1/mobs_redo",
+        dir = "",
+        def_branch = "master"
+    }
+}
+
+return {
+    repos = repos
+}
